583d52cad0a5e6fed09556b44ed7713e30b6a658
librairum <ivan.atanacio@gmail.com>
2025-04-30 23:07:07 -0500

    actualizacion

---
+++ CajaBanco.Repository/ConexionBD.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajaBanco.Repository
+{
+    public class ConexionBD
+    {
+
+        private string _cadena = "";
+        SqlConnection cn ;
+        public ConexionBD(IConfiguration configuracion)
+        {
+            this._cadena =  configuracion.GetConnectionString("conexion");
+        }
+        internal void Conectar() {
+            cn = new SqlConnection(this._cadena);
+            if(cn.State == System.Data.ConnectionState.Closed)
+            {
+                cn.Open();
+            }
+            
+        }
+        internal void Desceonctar() {
+            cn = new SqlConnection(this._cadena);
+
+            if(cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+            }
+            
+        }
+        internal void EjecutarProcedimientos(string[] parametros, string nombreProcedimiento)
+        {
+            
+        }
+
+    }
+}
